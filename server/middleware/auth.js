8a957e825eba553d239fb03da23930f23fa657ee
AnujKshatriya <anujkshatriyaiitian@gmail.com>
2024-07-16 20:08:19 +0530

    authentication added

---
+++ server/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+const AuthMiddleWare = async(req,res,next) =>{
+    const {token} = req.headers;
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Not Authorized to Log" });
+    }
+    try {
+        console.log("Token Recieved: ",token)
+
+        const decode = jwt.verify(token, process.env.JWT_key)
+        req.body.userId = decode.id;
+        next();
+    } catch (error) {
+        console.error("JWT Verification Error:", error); 
+
+        //Additional details about Error..if...
+        if (error.name === 'TokenExpiredError') {
+          return res.status(401).json({ success: false, message: "Token expired" });
+        } else if (error.name === 'JsonWebTokenError') {
+          return res.status(401).json({ success: false, message: "Invalid token" });
+        } else {
+          return res.status(500).json({ success: false, message: "Internal server error" });
+        }
+  
+    }
+}
+
+export default AuthMiddleWare;
