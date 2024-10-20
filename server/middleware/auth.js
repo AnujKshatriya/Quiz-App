d9f4ea8fa4c37e280600373130683fe39e1aff61
AnujKshatriya <anujkshatriyaiitian@gmail.com>
2024-10-21 04:29:11 +0530

    imporved authorization headers

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const AuthMiddleWare = async(req,res,next) =>{
-    const token = req.cookies.token;
+    const token = req.headers.authorization?.split(' ')[1]; 
 
     if (!token) {
         return res.status(401).json({ success: false, message: "Please Login First" });
